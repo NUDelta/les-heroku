3d1a7521e26a267f095c86274f1ba21c485c37f6
Kapil Garg <kapil1garg@gmail.com>
2018-03-05 15:17:38 -0600

    fix

--- cloud/main.js
+++ cloud/main.js
@@ -286,9 +286,9 @@ Parse.Cloud.define('retrieveLocations', (request, response) => {
   let includeWithoutPref; // include locations where preferred info is unavailable
 
   // 4X
-  // includeDistance = true;
-  // includeEnRoute = true;
-  // includeWithoutPref = false;
+  includeDistance = true;
+  includeEnRoute = true;
+  includeWithoutPref = false;
 
   // // opp at location
   // includeDistance = false;
@@ -296,9 +296,9 @@ Parse.Cloud.define('retrieveLocations', (request, response) => {
   // includeWithoutPref = false;
 
   // opp at distance
-  includeDistance = true;
-  includeEnRoute = false;
-  includeWithoutPref = true;
+  // includeDistance = true;
+  // includeEnRoute = false;
+  // includeWithoutPref = true;
 
   const lat = request.params.latitude;
   const lng = request.params.longitude;
